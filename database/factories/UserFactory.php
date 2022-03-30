dae10076c808fc6298c626edd2415a0a9d57661f
phuongbnv <vanphuong.190997@gmail.com>
2022-03-31 00:16:34 +0700

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('12345'), // password
+            'password' => Hash::make('1234'), // password
             'remember_token' => Str::random(10),
         ];
     }
